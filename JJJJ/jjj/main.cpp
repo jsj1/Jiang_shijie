68631182e1de19900a98fcab427a9ee72894e087
JSJ520\独 <3030453723@qq.com>
2019-03-17 16:07:35 +0800

    F:\haha\JJJJ\jjj\main.cpp

--- JJJJ/jjj/main.cpp
+++ JJJJ/jjj/main.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
 
-using namespace std
-;int main()
-{   double a = 0;
-    double b = 0;
-    int he = 0;
-    int cha = 0;
-    int ji = 0;
-    double shang = 0;
-    cout << "Enter two integers:";
-    cin  >> a >> b;
-    he = a+b;
-    cha = a-b;
-    ji = a*b;
-    shang = a/b;
-    cout << " he is " << he <<::endl;
-    cout << " cha is " << cha <<::endl;
-    cout << " ji is " << ji <<::endl;
-    cout << " shang is " << shang <<::endl;
+using namespace std;
 
-
+int main()
+{
+    cout <<"1, 2, 3, 4,";
+    cout <<" 1 " <<" 2 " <<" 3 " <<" 4 ";
+    cout <<" 1 ";
+    cout <<" 2 ";
+    cout <<" 3 ";
+    cout <<" 4 ";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
