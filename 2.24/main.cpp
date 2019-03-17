f6e6d8dd3e981a73718d140c21f428ab789dff1b
JSJ520\独 <3030453723@qq.com>
2019-03-17 20:18:59 +0800

    作业提交

---
+++ 2.24/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{  int a;
+   cout <<"Enter one integer:";
+   cin >>a;
+   if(a%2==0)
+   cout <<"输出a为偶数";
+   if(a&2!=0)
+    cout <<"输出a奇数";
+
+
+
+}
