526f51267eab564dfeaaa88569f1efba8b76125b
JSJ520\独 <3030453723@qq.com>
2019-03-17 17:23:42 +0800

    作业提交

---
+++ 2.16/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "enter number : " << endl;
+    double a , b;
+    double he , jian , chen ,shang ;
+    cin>>a>>b;
+    he = a + b;
+    jian = a - b;
+    chen = a * b;
+    shang = a / b;
+    cout << " he is : " << he <<endl;
+    cout << " jian is : " << jian <<endl;
+    cout << " chen is : " << chen <<endl;
+    cout << " shang is : " << shang <<endl;
+
+    return 0;
+}
---
+++ 2.20/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double pi=3.14159;
+    int r;
+    cout << " enter r : ";
+    cin >> r;
+    cout << "2r = " << 2*r <<endl;
+    cout << "C = " << 2*pi*r <<endl;
+    cout << "S = " << pi*r*r <<endl;
+
+    return 0;
+}
