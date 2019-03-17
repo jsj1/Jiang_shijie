d691e2cb2f57a3a1f6fc5abc49ea7d8dfbc4faca
JSJ520\独 <3030453723@qq.com>
2019-03-17 18:11:29 +0800

    作业提交

---
+++ 2.18/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a,b;
+   cout <<"Enter two integers to compare:";
+   cin >>a>>b;
+   if(a>b)
+   cout << "a is large";
+   if(a<b)
+   cout <<" b is large";
+   if(a==b)
+    cout <<"These numbers are equal";
+}
